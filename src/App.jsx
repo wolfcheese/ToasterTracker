b69cb0de125714e524a5a4c679f28c3550d5abef
wolfcheese <wolfcheese@gmail.com>
2026-07-16 21:41:17 -0400

    Updated node version

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ export default function GameApp() {
   const [game, setGame] = useState(gameData);
 
   return (
-    <div className="card mx-auto" style={{ maxWidth: '420px' }}>
+    <div className="card mx-auto" style={{ maxWidth: '1028px' }}>
       <div className="card-body">
         <h2 className="card-title">New Game</h2>
         <div className="d-flex align-items-center justify-content-between">
-          {game.players.map((player, index) => (
-            <div key={index} className="text-center">
-              <h3>{player.name}</h3>
+          {game.players.map((gamePlayer, index) => (
+            <div key={gameplayer.id} className="text-center">
+              <span>{gamePlayer.player.name}</span>
             </div>
           ))}
         </div>
